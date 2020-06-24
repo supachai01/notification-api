656aa640cd06afcdcd97a29373bb41b87db450ac
saman.moshafi <saman.moshafi@thoughtworks.com>
2020-06-24 15:16:42 -0400

    [#51] add conditionals for edge cases when incrementing patch version for tag
    Co-authored-by: Ling Tran <ltran@thoughtworks.com>

--- scripts/increment_tag.sh
+++ scripts/increment_tag.sh
@@ -3,13 +3,22 @@
 function increase_patch_number () {
   patch_version=`echo $1 | sed 's/\(.*[0-9]\.\)\([0-9]\)/\2/'`
   first_portion=`echo $1 | sed 's/\(.*[0-9]\.\)\([0-9]\)/\1/'`
-  ((patch_version++))
-  echo $first_portion$patch_version
+
+  if [[ $patch_version =~ [0-9] ]];then
+    echo "Invalid patch version: $1"
+    exit 1
+  else
+    ((patch_version++))
+    result=$first_portion$patch_version
+  fi
+  echo $result
 }
 
 function get_latest_version_tag () {
-#  local latest_version_tag=`git tag -l v*`
   local latest_version_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+  if [ -z "$latest_version_tag" ]; then
+    latest_version_tag="rc-0.0.0"
+  fi
   echo $latest_version_tag
 }
 
