7bfdf53fc045cd91f33bb9a94e6ea64b3e69599c
saman.moshafi <saman.moshafi@thoughtworks.com>
2020-06-24 12:23:23 -0400

    [#51] add version script
    Co-authored-by: Ling Tran <ltran@thoughtworks.com>

--- scripts/increment_tag.sh
+++ scripts/increment_tag.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 function increase_patch_number () {
-  patch_version=`echo $1 | tr -d [:alpha:] | sed 's/\(.*\.\)\([0-9]\)/\2/'`
-  first_portion=`echo $1 | tr -d [:alpha:] | sed 's/\(.*\.\)\([0-9]\)/\1/'`
+  patch_version=`echo $1 | sed 's/.*\(.[0-9]\)/\1/'`
+  first_portion=`echo $1 | sed 's/\(.*\).[0-9]/\1/'`
   ((patch_version++))
-  echo 'v'$first_portion$patch_version
+  echo $first_portion$patch_version
 }
 
 function get_latest_version_tag () {
-  local latest_version_tag=`git tag -l v*`
-  # git describe --tags --match v*
+#  local latest_version_tag=`git tag -l v*`
+  local latest_version_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
   echo $latest_version_tag
 }
 
